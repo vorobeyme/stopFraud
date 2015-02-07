817cf22ed545d983c52da146f6b478983a8fab32
galyna <galyna.dev@gmail.com>
2015-02-07 20:31:04 +0200

    refacrot pointsController routing config

--- project/src/ApiBundle/Controller/PointsController.php
+++ project/src/ApiBundle/Controller/PointsController.php
@@ -3,37 +3,62 @@
 namespace ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+/**
+ * @Route("/")
+ */
 class PointsController extends Controller
 {
+    /**
+     * @Route("/point_status/long/{long}/lat/{lat}", requirements={"long" = "\d+", "lat" = "\d+"})
+     * @Method("GET")
+     * @Template
+     */
     public function getPointStatusAction(Request $request)
     {
         $long = (int)$request->attributes->get('long');
         $lat  = (int)$request->attributes->get('lat');
 
-        $pointLocationStatus = $this->getDoctrine()->getManager()
-            ->getRepository('ApiBundle:PointLocation')
-            ->findValidPoint($long, $lat);
+        try {
+            $pointLocationStatus = $this->getDoctrine()->getManager()
+                ->getRepository('ApiBundle:PointLocation')
+                ->findValidPoint($long, $lat);
 
+            return new JsonResponse([
+                'status' => $pointLocationStatus
+            ]);
 
-        return new JsonResponse([
-            'status' => $pointLocationStatus
-        ]);
+        } catch (Exception $e) {
+            return new JsonResponse(['message' => 'server error']);
+        }
     }
 
+    /**
+     * @Route("/points/long/{long}/lat/{lat}", requirements={"long" = "\d+", "lat" = "\d+"})
+     * @Method("GET")
+     * @Template
+     */
     public function getPointsAction(Request $request)
     {
         $long = (int)$request->attributes->get('long');
         $lat  = (int)$request->attributes->get('lat');
 
-        $pointLocationStatus = $this->getDoctrine()->getManager()
-            ->getRepository('ApiBundle:PointLocation')
-            ->findValidPoints($long, $lat);
+        try {
+            $pointLocationStatus = $this->getDoctrine()->getManager()
+                ->getRepository('ApiBundle:PointLocation')
+                ->findValidPoints($long, $lat);
 
+            return new JsonResponse($pointLocationStatus);
 
-        return new JsonResponse([
-            'status' => $pointLocationStatus
-        ]);
-    }}
+        } catch (Exception $e) {
+            return new JsonResponse(['message' => 'server error']);
+        }
+    }
+}
